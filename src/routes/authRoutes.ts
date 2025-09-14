143ea235f944d2b84a1fe5d638ac74de3543c6d4
ahmednaser111 <ahmednaser7707@gmail.com>
2025-09-14 19:07:55 +0300

    style: format code for consistency in authRoutes.ts

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,15 +1,14 @@
-import { Router } from 'express';
-import AuthController from '../controllers/authController';
-import AuthMiddleware from '../middlewares/authMiddleware';
+import { Router } from "express";
+import AuthController from "../controllers/authController";
+import AuthMiddleware from "../middlewares/authMiddleware";
 
 const router = Router();
-
-router.post('/auth/register', AuthController.signup);
-router.post('/auth/login', AuthController.login);
+router.post("/auth/register", AuthController.signup);
+router.post("/auth/login", AuthController.login);
 router.get(
-  '/user/:id',
-  AuthMiddleware.authenticate,
-  AuthController.getUserById
+	"/user/:id",
+	AuthMiddleware.authenticate,
+	AuthController.getUserById
 );
 
 export default router;
